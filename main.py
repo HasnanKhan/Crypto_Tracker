8b6effbb66aa211df3bfa8ba0d6c9fe1f8c1066d
HasnanKhan <90119809+HasnanKhan@users.noreply.github.com>
2025-07-24 19:55:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ if __name__ == "__main__":
     btc_change = b['Data']['BTC-USD']['CURRENT_HOUR_CHANGE']
     hbar_change = h['Data']['HBAR-USD']['CURRENT_HOUR_CHANGE']
 
-    if abs(xrp_change) > .02:
-        s += 'XRP: ' + str(x['Data']['XRP-USD']['PRICE']) + '\n'
+    if abs(xrp_change) > 0.01:
+        s += 'XRP: ' + str(x['Data']['XRP-USD']['PRICE']) + " " + "Change: " + str(xrp_change) + '\n'
     if abs(btc_change) > 1000:
-        s += 'BTC: ' + str(b['Data']['BTC-USD']['PRICE']) + '\n'
+        s += 'BTC: ' + str(b['Data']['BTC-USD']['PRICE']) + " " + "Change: " + str(btc_change) + '\n'
     if abs(hbar_change) > 0.01:
-        s += 'HBAR: ' + str(h['Data']['HBAR-USD']['PRICE']) + '\n'
+        s += 'HBAR: ' + str(h['Data']['HBAR-USD']['PRICE']) + " " + "Change: " + str(hbar_change) + '\n'
 
     if s:
         requests.post('https://textbelt.com/text', {
