c233f5b90046840f37415b82b5afa8d2ffb33f22
Hasnan Khan <hasnan@Hasnans-MacBook-Pro.local>
2025-07-24 16:16:34 -0400

    working script

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+COINDESK_API_KEY = os.getenv("COINDESK_API_KEY")
+TEXTBELT_KEY = os.getenv("TEXTBELT_KEY")
+PHONE_NUMBER = os.getenv("PHONE_NUMBER")
+
+def xrp():
+    url = f"https://data-api.coindesk.com/spot/v1/latest/tick?apply_mapping=true&market=coinbase&instruments=XRP-USD&groups=CURRENT_HOUR,VALUE&api_key={COINDESK_API_KEY}"
+    return requests.get(url)
+
+def btc():
+    url = f"https://data-api.coindesk.com/spot/v1/latest/tick?apply_mapping=true&market=coinbase&instruments=BTC-USD&groups=CURRENT_HOUR,VALUE&api_key={COINDESK_API_KEY}"
+    return requests.get(url)
+
+def hbar():
+    url = f"https://data-api.coindesk.com/spot/v1/latest/tick?apply_mapping=true&market=coinbase&instruments=HBAR-USD&groups=CURRENT_HOUR,VALUE&api_key={COINDESK_API_KEY}"
+    return requests.get(url)
+
+if __name__ == "__main__":
+    x = xrp().json()
+    b = btc().json()
+    h = hbar().json()
+    s = ""
+
+    xrp_change = x['Data']['XRP-USD']['CURRENT_HOUR_CHANGE']
+    btc_change = b['Data']['BTC-USD']['CURRENT_HOUR_CHANGE']
+    hbar_change = h['Data']['HBAR-USD']['CURRENT_HOUR_CHANGE']
+
+    if abs(xrp_change) > 0.05:
+        s += 'XRP: ' + x['Data']['XRP-USD']['PRICE'] + '\n'
+    if abs(btc_change) > 1000:
+        s += 'BTC: ' + b['Data']['BTC-USD']['PRICE'] + '\n'
+    if abs(hbar_change) > 0.01:
+        s += 'HBAR: ' + h['Data']['HBAR-USD']['PRICE'] + '\n'
+
+    if s:
+        requests.post('https://textbelt.com/text', {
+            'phone': PHONE_NUMBER,
+            'message': s,
+            'key': TEXTBELT_KEY,
+        })
